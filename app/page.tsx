fc9222c65f22e81a58b96c957c4315eaa820892c
brianxia273 <briangxia@gmail.com>
2025-11-18 19:33:23 -0500

    Temporary fix for React Router
    - Will have to change to Next.js routing in the future

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-import AppRouter from "./components/RouterProvider";
+"use client"
+import dynamic from "next/dynamic";
+
+const AppRouter = dynamic(() => import("./components/RouterProvider"), {
+  ssr: false,
+});
+
 
 export default function Page() {
   return <AppRouter />;
